b8a5726a39b83984ec1908c7ee1b2145504670dc
Matthew Taube <taubemj@gmail.com>
2019-05-14 17:17:42 -0400

    Updates

---
+++ precache-manifest.a6048a83d75c2429a7f8d4722e7256b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe2c604d6825a0a02cd2bff85ba641f",
+    "url": "/Clicky-game/index.html"
+  },
+  {
+    "revision": "0e8092ea666fcfe0aef7",
+    "url": "/Clicky-game/static/css/main.626cad2d.chunk.css"
+  },
+  {
+    "revision": "ff2b54d40a6d618c8c20",
+    "url": "/Clicky-game/static/js/2.5996a945.chunk.js"
+  },
+  {
+    "revision": "0e8092ea666fcfe0aef7",
+    "url": "/Clicky-game/static/js/main.438a6c00.chunk.js"
+  },
+  {
+    "revision": "cda0924bd23e3d9029fa",
+    "url": "/Clicky-game/static/js/runtime~main.952307d2.js"
+  }
+]);
